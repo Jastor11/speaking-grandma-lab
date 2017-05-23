dbd97dc7e37c3c3fa1d8e4ada5e991ffb9e16812
Jeff Astor <jastor@laalliance.org>
2017-05-23 15:06:00 -0700

    Update grandma.rb

--- grandma.rb
+++ grandma.rb
@@ -8,15 +8,3 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
-
-def speak_to_grandma(talk)
-  if talk === talk.upcase
-    "NO, NOT SINCE 1938!"
-  else   
-    "HUH?! SPEAK UP, SONNY!"
-  end 
-end
-
-puts speak_to_grandma("Wassup granny?")
-puts speak_to_grandma("I'm talking to you granny?")
-puts speak_to_grandma("GRAMMMMYYYYY DO YOU PARTY?")
